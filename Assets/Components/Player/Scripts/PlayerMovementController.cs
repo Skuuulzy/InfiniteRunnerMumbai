6ef2c25df3300f419be7f734ff33b055201d1a0d
Skuuulzy <yona.rutkowski@gmail.com>
2026-02-25 20:20:35 +0100

    Player Jump Behaviour
    - Add mini market 3D assets
    - Start working on player movement: adding jump with coroutine

---
+++ Assets/Components/Player/Scripts/PlayerMovementController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    [SerializeField] private float _jumpDuration = 1f;
+    [SerializeField] private float _jumpHeight = 2f;
+    [SerializeField] private AnimationCurve _jumpCurve;
+    [SerializeField] private AnimationCurve _fallCurve;
+
+    public void Update()
+    {
+        if (Keyboard.current.upArrowKey.wasPressedThisFrame)
+        {
+            StartCoroutine(JumpCoroutine());
+        }
+    }
+
+    private IEnumerator JumpCoroutine()
+    {
+        float jumpTimer = 0f;
+        float halfJumpDuration = _jumpDuration / 2f;
+
+        // Jump
+        while (jumpTimer < halfJumpDuration)
+        {
+            jumpTimer += Time.deltaTime;
+            var normalizedTime = jumpTimer / halfJumpDuration;
+
+            var targetHeight = _jumpCurve.Evaluate(normalizedTime) * _jumpHeight;
+            var targetPosition = new Vector3(transform.position.x, targetHeight, transform.position.z);
+
+            transform.position = targetPosition;
+
+            yield return null;
+        }
+        
+        // Fall
+        jumpTimer = 0f;
+        
+        while (jumpTimer < halfJumpDuration)
+        {
+            jumpTimer += Time.deltaTime;
+            var normalizedTime = jumpTimer / halfJumpDuration;
+
+            var targetHeight = _fallCurve.Evaluate(normalizedTime) * _jumpHeight;
+            var targetPosition = new Vector3(transform.position.x, targetHeight, transform.position.z);
+
+            transform.position = targetPosition;
+
+            yield return null;
+        }
+    }
+}
