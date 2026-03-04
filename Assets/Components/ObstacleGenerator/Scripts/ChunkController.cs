d9dc06584d8c3d286952f975219d808a2153a019
Skuuulzy <yona.rutkowski@gmail.com>
2026-03-04 19:48:49 +0100

    Add dynamic chunk instanciation

--- Assets/Components/ObstacleGenerator/Scripts/ChunkController.cs
+++ Assets/Components/ObstacleGenerator/Scripts/ChunkController.cs
@@ -5,4 +5,9 @@ public class ChunkController : MonoBehaviour
     [SerializeField] private Transform _endAnchor;
     
     public Vector3 EndAnchor => _endAnchor.position;
+
+    public bool IsBehindPlayer()
+    {
+        return EndAnchor.z <= 0;
+    }
 }
