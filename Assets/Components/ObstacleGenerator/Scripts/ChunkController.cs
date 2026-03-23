614b3281ef1ec6faae64cd0e84c3b96b3c2bb4d5
Skuuulzy <yona.rutkowski@gmail.com>
2026-03-23 20:33:51 +0100

    WIP Enemy
    Add a wip enemy that throw projectile at the player

--- Assets/Components/ObstacleGenerator/Scripts/ChunkController.cs
+++ Assets/Components/ObstacleGenerator/Scripts/ChunkController.cs
@@ -3,9 +3,20 @@ using UnityEngine;
 public class ChunkController : MonoBehaviour
 {
     [SerializeField] private Transform _endAnchor;
+    [SerializeField] private EnnemyController _enemyPrefab;
+    [SerializeField] private float _enemyProbability = 0.3f;
     
     public Vector3 EndAnchor => _endAnchor.position;
 
+    private void Start()
+    {
+        var random = Random.Range(0, 1f);
+        if (random < _enemyProbability)
+        {
+            Instantiate(_enemyPrefab, transform);
+        }
+    }
+    
     public bool IsBehindPlayer()
     {
         return EndAnchor.z <= 0;
